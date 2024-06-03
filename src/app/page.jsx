323e28ef7fd80c19c2a0d9ae93d4f62cb6205d99
Jeronimo <jeronimotornini@gmail.com>
2024-06-03 16:18:30 -0300

    Fix time tag prop issue

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -13,7 +13,7 @@ export default function Home() {
         <aside className="p-4 flex flex-col gap-3 text-pretty">
           <h1 className="text-2xl font-medium">Lorem ipsum dolor sit amet.</h1>
           <span className="text-zinc-600">
-            Creada el: <time datetime="2024/06/02">2024/06/02</time>
+            Creada el: <time dateTime="2024/06/02">2024/06/02</time>
           </span>
           <p className="leading-loose mt-10 text-xl font-light">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt atque
