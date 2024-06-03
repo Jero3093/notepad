14231926b7f2f22a830b60072016abe9903b865e
Jeronimo <jeronimotornini@gmail.com>
2024-06-03 15:50:13 -0300

    Add supabase client configuration file

---
+++ src/utils/supabase/client.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_PROJECT_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+const supabaseClient = createClient(supabaseUrl, supabaseKey);
+
+export default supabaseClient;
