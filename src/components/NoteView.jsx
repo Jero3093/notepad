211c07e069fc29e9d290e498859021f02fd45785
Jeronimo <jeronimotornini@gmail.com>
2024-07-02 13:27:00 -0300

    Fix note buttons and title for mobile view

--- src/components/NoteView.jsx
+++ src/components/NoteView.jsx
@@ -7,20 +7,20 @@ const NoteView = ({ id, title, content, created_at }) => {
 
   return (
     <aside className="p-4 flex flex-col gap-3 text-pretty pb-36 overflow-y-auto w-full lg:h-full">
-      <header className="flex flex-row justify-between items-center">
+      <header className="flex flex-row justify-between md:items-center">
         <aside className="flex flex-col gap-3">
-          <h1 className="text-2xl font-medium md:text-3xl">{title}.</h1>
+          <h1 className="text-2xl font-medium md:text-3xl line-clamp-2 max-w-80 md:max-w-lg lg:max-w-2xl">{title}.</h1>
           <span className="text-zinc-600">
             Creada el: <time dateTime={created_date}>{created_date}</time>
           </span>
         </aside>
-        <aside className="flex flex-row gap-3">
+        <aside className="flex flex-col gap-3 md:flex-row">
           <ExportNoteButton title={title} content={content} />
           <EditNoteButton noteId={id} />
           <DeleteNoteButton noteId={id} />
         </aside>
       </header>
-      <pre className="leading-loose mt-10 text-xl font-[system-ui] text-wrap">
+      <pre className="leading-loose text-xl font-[system-ui] text-wrap md:mt-10">
         {content}
       </pre>
     </aside>
