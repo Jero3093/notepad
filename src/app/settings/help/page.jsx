095c2d379a32d783f5c2a6a193da2570072d140a
Jeronimo <jeronimotornini@gmail.com>
2024-08-03 17:21:06 -0300

    Add help page with instructions about the app

---
+++ src/app/settings/help/page.jsx
@@ -0,0 +1,61 @@
+import { redirect } from "next/navigation";
+import { Toaster } from "sonner";
+import Header from "@/components/Header";
+import Nav from "@/components/Nav";
+import NewNoteButton from "@/components/NewNoteButton";
+import useSession from "@/hooks/useSession";
+import useNotes from "@/hooks/useNotes";
+import { HelpInstructions } from "@/constants/Help";
+
+export default async function Help() {
+  const session = await useSession();
+
+  if (!session) redirect("/auth/signin");
+
+  const user = await useUser({ session });
+
+  const notes = await useNotes({ userId: user[0]?.id });
+
+  return (
+    <main className="flex flex-col h-screen">
+      <Toaster richColors position="top-center" />
+      <Header />
+
+      <section className="h-full flex flex-col lg:flex-row">
+        <Nav notes={notes} />
+
+        <aside className="flex flex-col gap-10 text-pretty w-full h-full p-4 overflow-auto pb-20">
+          <header className="flex flex-col gap-5">
+            <h2 className="text-2xl font-medium">
+              Nombre de la Aplicación: {HelpInstructions?.applicationName}
+            </h2>
+            <h3 className="text-xl font-medium">
+              Versión Actual: {HelpInstructions?.version}
+            </h3>
+          </header>
+
+          <section className="flex flex-col gap-5">
+            <h4 className="text-2xl font-semibold">Instrucciones</h4>
+            {HelpInstructions?.instructions.map(
+              ({ step, title, description }) => {
+                return (
+                  <div
+                    className="flex flex-col gap-4 text-lg font-light leading-relaxed"
+                    key={title}
+                  >
+                    <header className="flex flex-row gap-2 items-center">
+                      <span className="font-semibold">{step}</span> -
+                      <h5 className="font-medium">{title}</h5>
+                    </header>
+                    {description}
+                  </div>
+                );
+              }
+            )}
+          </section>
+        </aside>
+      </section>
+      <NewNoteButton />
+    </main>
+  );
+}
---
+++ src/constants/Help.js
@@ -0,0 +1,45 @@
+export const HelpInstructions = {
+  applicationName: "Notepad",
+  version: "1.4",
+  instructions: [
+    {
+      step: 1,
+      title: "Crear una nueva nota",
+      description:
+        "Puedes crear una nueva nota de dos maneras: haz clic en el botón en la parte inferior izquierda en todas las páginas o en la página de inicio, cliquea el botón 'Agregar Nota'.",
+    },
+    {
+      step: 2,
+      title: "Escribir una nota",
+      description:
+        "Ingresa el título y el contenido de tu nota en los campos de texto obligatorios proporcionados.",
+    },
+    {
+      step: 3,
+      title: "Guardar la nota",
+      description: "Haz clic en el botón 'Guardar Nota' para guardar tu nota.",
+    },
+    {
+      step: 4,
+      title: "Editar una nota",
+      description:
+        "Para editar una nota existente, selecciona la nota de la lista y haz clic en el boton que lleva un 'lapiz' en la parte superior derecha en la pagina de la nota.",
+    },
+    {
+      step: 5,
+      title: "Eliminar una nota",
+      description:
+        "Para eliminar una nota, selecciona la nota de la lista y haz clic en el boton que lleva un 'bote de basura' en la parte superior derecha en la pagina de la nota, luego aparecera una alerta en la parte superior, si quiere eliminar la nota haga clic en el boton de la alerta que dice 'Eliminar', si no quiere eliminarla solo deje que la alerta desaparezca.",
+    },
+    {
+      step: 6,
+      title: "Buscar notas",
+      description:
+        "Usa la barra de búsqueda en la barra lateral izquierda, si no cuenta con esta barra de busqueda eso significa que no tiene ninguna nota creada, si ya tiene notas creadas solo debe escribir el titulo de la nota que quiere buscar y se le mostraran los resultados",
+    },
+  ],
+  contactSupport: {
+    email: "jerotornini@outlook.com",
+    phone: "+59891800933",
+  },
+};
