435f1cc44783e895f84f858cb13bc7bb57d540ad
Jeronimo <jeronimotornini@gmail.com>
2024-09-07 21:49:29 -0300

    Update cookies route to set maxAge to session cookie

--- src/app/api/auth/cookies/route.js
+++ src/app/api/auth/cookies/route.js
@@ -1,16 +1,14 @@
 import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
 
 export async function POST(request) {
   const data = await request.json();
 
   const session = JSON.stringify(data);
 
-  const response = NextResponse.json({ session });
-
-  response.cookies.set({
-    name: "session",
-    value: session,
+  cookies().set("session", session, {
+    maxAge: 432000,
   });
 
-  return response;
+  return NextResponse.json({ status: 200 });
 }
